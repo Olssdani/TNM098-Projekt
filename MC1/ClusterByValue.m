d8108693956d2d6d628a3a4b52d123479c4bd20d
Daniel <daniel.olsson1992@gmail.com>
2019-05-16 08:43:45 +0200

    fixed mean per zon/hour

--- MC1/ClusterByValue.m
+++ MC1/ClusterByValue.m
@@ -1,12 +1,12 @@
 function [ClusterData] = ClusterByValue(Info)
     % Cluster the data by a certain threshold or a limit
     %Limits for the three different groups
-    GroupLimits = [3 6];
+    GroupLimits = [5 7];
     %Level for the emergency 
-    EmercencyNr = 8;
+    EmercencyNr = 9;
 
     %Wheight measurement road, medical, intensity, water, building, power
-    weight = [2/12 4/12 1/12, 2/12 1/12 2/12 ];
+    weight = [2/12 2/12 2/12, 2/12 2/12 2/12 ];
 
 
     for i=1:size(Info, 1)
