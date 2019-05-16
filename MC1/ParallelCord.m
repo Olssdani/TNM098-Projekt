ec8b09e4f71ce13a016b998b1bb6103136ae4313
Daniel <daniel.olsson1992@gmail.com>
2019-05-16 09:52:53 +0200

    Image analyise

--- MC1/ParallelCord.m
+++ MC1/ParallelCord.m
@@ -1,19 +1,10 @@
 function ParallelCord(Info)
     
     labels = {'Road','Medical','Intensity','Water', 'Building', 'Power'};
-    
-%     for i= 1:25
-%         x(1,i) =Info(i).Road;
-%         x(2,i) =Info(i).Medical;
-%         x(3,i) =Info(i).Intensity;
-%         x(4,i) =Info(i).Water;
-%         x(5,i) =Info(i).Building;
-%         x(6,i) =Info(i).Power;
-%     end
-%     x = x';
+
 
     groups = ClusterByValue(Info);
-    parallelcoords(Info,'Group', groups,'Labels',labels);
+    parallelcoords(Info,'Group', groups,'Labels',labels,'LineWidth',2);
     
     
 end
